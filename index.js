aada7af1def7c91f7242f3ee6d95582df2e2bfaa
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2020-07-29 10:06:50 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+<input id="finalanswer" name="answers" type="text" /> <br />
+
+<input onclick="formCalci.answers.value += '1' " type="button" value="1" />
+<input onclick="formCalci.answers.value += '2' " type="button" value="2" />
+<input onclick="formCalci.answers.value += '3' " type="button" value="3" />
+
+<br />
+
+
+<input onclick="formCalci.answers.value += '4' " type="button" value="4" />
+<input onclick="formCalci.answers.value += '5' " type="button" value="5" />
+<input onclick="formCalci.answers.value += '6' " type="button" value="6" />
+<br />
+
+<input onclick="formCalci.answers.value += '7' " type="button" value="7" />
+<input onclick="formCalci.answers.value += '8' " type="button" value="8" />
+<input onclick="formCalci.answers.value += '9' " type="button" value="9" />
+
+<br />
+
+<input onclick="formCalci.answers.value += '+' " type="button" value="+" />
+<input onclick="formCalci.answers.value += '-' " type="button" value="-" />
+<input onclick="formCalci.answers.value += '/' " type="button" value="/" />
+
+<br />
+
+<input onclick="formCalci.answers.value += '0' " type="button" value="0" />
+<input onclick="formCalci.answers.value += '*' " type="button" value="*" />
+<input onclick="formCalci.answers.value = eval(formCalci.answers.value) " type="button" value="=" />
+<br />
+
